c433119f1ca5d893b78ca11efb4b2c6e2ba4da56
daniela <dggtn97@gmail.com>
2024-12-09 20:54:11 -0300

    agrego clase reactorNuclear

---
+++ src/ReactorNuclear.java
@@ -0,0 +1,17 @@
+public class ReactorNuclear {
+    private int aporte;
+
+    public ReactorNuclear(int aporte) {
+        this.aporte = aporte;
+    }
+
+    public int getAporte() {
+        return aporte;
+    }
+
+    public void setAporte(int aporte) {
+        this.aporte = aporte;
+    }
+
+
+}

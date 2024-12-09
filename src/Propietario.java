616fca19d61bec748eb39ec0a5ca9e79e503a6fb
daniela <dggtn97@gmail.com>
2024-12-09 20:53:42 -0300

    agrego clase propietario

---
+++ src/Propietario.java
@@ -0,0 +1,11 @@
+public class Propietario {
+    private int cuit;
+    private String nombre;
+    private int cantCentrales;
+
+    public Propietario(int cuit, String nombre, int cantCentrales) {
+        this.cuit = cuit;
+        this.nombre = nombre;
+        this.cantCentrales = cantCentrales;
+    }
+}

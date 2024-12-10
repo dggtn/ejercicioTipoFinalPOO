bdb2ae8147cf9dfdcc882101189ab6d2ce12cc11
daniela <dggtn97@gmail.com>
2024-12-10 08:44:03 -0300

    agrego metodo modificarCantConsumoEnergia

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,76 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.List;
+import java.util.PropertyPermission;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        Main main = new Main();
+
+        main.probarConsumoTotal();
+        main.sinCentralesContaminantes();
+        main.crearCentralContaminante();
+        main.crearSiEsoNoEsEficiente();
     }
-}
+    private void crearSiEsoNoEsEficiente(){
+        Propietario propietario = new Propietario(1, "Pepito", 0);
+        Ciudad buenosAires = new Ciudad("Buenos Aires");
+        CentralNuclear central = new CentralNuclear(100,propietario);
+        ReactorNuclear x = new ReactorNuclear(1);
+
+        buenosAires.agregarCentral(central);
+        CentralNuclear c = new CentralNuclear(145,propietario);
+        ReactorNuclear re = new ReactorNuclear(150);
+        c.agregarReactor(new ReactorNuclear(85));
+        c.agregarReactor(re);
+        buenosAires.agregarCentral(c);
+        central.agregarReactor(x);
+        System.out.println("Es buenos aires eficiente: "+buenosAires.esEficiente());
+
+    }
+    private void crearCentralContaminante(){
+        Propietario propietario = new Propietario(1, "Pepito", 0);
+        CentralNuclear central = new CentralNuclear(100,propietario);
+        ReactorNuclear r = new ReactorNuclear(10);
+        ReactorNuclear x = new ReactorNuclear(1);
+        ReactorNuclear y = new ReactorNuclear(10);
+        ReactorNuclear z = new ReactorNuclear(1);
+
+        central.agregarReactor(r);
+        central.agregarReactor(x);
+        central.agregarReactor(y);
+        central.agregarReactor(z);
+
+        System.out.println(central.esContaminante());
+
+        Ciudad buenosAires = new Ciudad("Buenos Aires");
+        buenosAires.agregarCentral(central);
+        List<CentralesProductoras> centralesContaminantes = buenosAires.obtnenerCentralesContaminantes();
+        System.out.println((centralesContaminantes.size()));
+    }
+
+    private void sinCentralesContaminantes() {
+        Propietario propietario = new Propietario(1, "Pepito", 0);
+        CentralesProductoras parqueEolico = new ParqueEolico(propietario, 100, 1);
+        Ciudad buenosAires = new Ciudad("Buenos Aires");
+        buenosAires.agregarCentral(parqueEolico);
+        List<CentralesProductoras> centralesContaminantes = buenosAires.obtnenerCentralesContaminantes();
+        System.out.println((centralesContaminantes.size()));
+    }
+
+    //PUNTO A
+    private void probarConsumoTotal() {
+        Consumidor uade = new Consumidor("UADE");
+        uade.modificarCantConsumEnergia(200);
+
+        Consumidor casa = new Consumidor("Casa");
+        casa.modificarCantConsumEnergia(100);
+
+        Ciudad buenosAires = new Ciudad("Buenos Aires");
+        buenosAires.agregarConsumidor(uade);
+        buenosAires.agregarConsumidor(casa);
+
+
+        System.out.println(buenosAires.consumoTotal());
+    }
+
+};

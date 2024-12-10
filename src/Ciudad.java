04333e9fb1c0be68d03195d62d03ec69b67fd8c7
daniela <dggtn97@gmail.com>
2024-12-10 08:42:53 -0300

    agrego metodo esPotencialmentePeligrosa

--- src/Ciudad.java
+++ src/Ciudad.java
@@ -1,74 +1,87 @@
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Ciudad {
     private String nombre;
     private List<CentralesProductoras> listaDeCentrales;
     private List<Consumidor> listaDeConsumidores;
-    private CentralesProductoras c;
 
 
     public Ciudad(String nombre) {
         this.nombre = nombre;
-    }
-
-    public void agregarCentral(CentralesProductoras c) {
-        listaDeCentrales.add(c);
-
-    }
-
-    public void agregarConsumidor(Consumidor c) {
-        listaDeConsumidores.add(c);
-
-    }
-
-    public int consumoTotalCiudad() {
-        int sumatoriaConsumoTotal = 0;
-        for (Consumidor c : listaDeConsumidores) {
-
-            sumatoriaConsumoTotal += c.getCantConsumEnergia();
-        }
-        return sumatoriaConsumoTotal;
-
+        this.listaDeCentrales = new ArrayList<>();
+        this.listaDeConsumidores = new ArrayList<>();
     }
 
     public boolean esEficiente() {
-        boolean es = false;
-        for (CentralesProductoras c : listaDeCentrales) {
-            if (!c.esEficiente()) {
-                es = false;
-            } else {
-                es = true;
+        boolean esEficiente = true;
+        for(CentralesProductoras central: listaDeCentrales) {
+            if (central.nivelEmision() >= 82) {
+                esEficiente = false;
             }
-
         }
-        return es;
+        return esEficiente;
+    }
+
+    public List<CentralesProductoras> obtnenerCentralesContaminantes() {
+        List<CentralesProductoras> contaminantes = new ArrayList<>();
+        for(CentralesProductoras central: listaDeCentrales) {
+            if (central.nivelEmision() >= 70) {
+                contaminantes.add(central);
+            }
+        }
+        return contaminantes;
+    }
+
+    public int consumoTotal() {
+        int consumoTotal = 0;
+        for (Consumidor consumidor: listaDeConsumidores) {
+            consumoTotal += consumidor.getCantConsumEnergia();
+        }
+        return consumoTotal;
+    }
+
+    public Set<Propietario> obtenerPropietarios() {
+        Set<Propietario> propietarios = new HashSet<>();
+        for (CentralesProductoras central: listaDeCentrales) {
+            propietarios.add(central.propietario);
+        }
+        return propietarios;
     }
 
     public boolean esSustentable() {
-        boolean es = false;
-        int sumatoriaProduccionTotal = 0;
-        int sumatoriaConsumoTotal = 0;
-        for (CentralesProductoras c : listaDeCentrales) {
-            sumatoriaProduccionTotal += c.cantEnergiaAaportar;
-        }
+        int consumoTotal = this.consumoTotal();
+        int produccionTotal = this.produccionTotal();
 
-        for (Consumidor c : listaDeConsumidores) {
-            sumatoriaConsumoTotal += c.getCantConsumEnergia();
-        }
-
-        if (sumatoriaProduccionTotal > sumatoriaConsumoTotal) {
-            es = true;
-
-        }
-
-
-        return es;
+        return consumoTotal < produccionTotal;
     }
 
-    public void esPeligrosa() {
-        boolean exigida = false;
-        for (CentralesProductoras c : listaDeCentrales) {
-            exigida = true;
+    private int produccionTotal() {
+        int produccionTotal = 0;
+        for (CentralesProductoras central: listaDeCentrales) {
+            produccionTotal += central.calcularEnergiaProducida();
         }
+        return produccionTotal;
+    }
+
+    public boolean esPotencialmentePeligrosa() {
+        boolean esPotencialmentePeligrosa = false;
+        for(CentralesProductoras centrales: listaDeCentrales) {
+            if (centrales.estaExigida()) {
+                esPotencialmentePeligrosa = true;
+            }
+        }
+
+        return esPotencialmentePeligrosa;
+    }
+
+    public void agregarConsumidor(Consumidor consumidor) {
+        this.listaDeConsumidores.add(consumidor);
+    }
+
+    public void agregarCentral(CentralesProductoras central) {
+        this.listaDeCentrales.add(central);
     }
 }

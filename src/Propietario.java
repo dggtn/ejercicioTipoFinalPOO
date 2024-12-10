ed9bd4f635b3b83659bc827aa7a2ff0805412fae
daniela <dggtn97@gmail.com>
2024-12-10 08:46:12 -0300

    agrego metodo equals para comparar por cuit

--- src/Propietario.java
+++ src/Propietario.java
@@ -1,3 +1,5 @@
+import java.util.Objects;
+
 public class Propietario {
     private int cuit;
     private String nombre;
@@ -7,5 +9,23 @@ public class Propietario {
         this.cuit = cuit;
         this.nombre = nombre;
         this.cantCentrales = cantCentrales;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Propietario that = (Propietario) o;
+        return cuit == that.cuit;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(cuit);
+    }
+
+    @Override
+    public String toString() {
+        return "Propietario[CUIT:" + this.cuit + "]";
     }
 }

35ef0f4ab3bf97aed3df1717202e02da221961c5
daniela <dggtn97@gmail.com>
2024-12-10 08:45:10 -0300

    agrego metodos incrementar decrementar generadores

--- src/ParqueEolico.java
+++ src/ParqueEolico.java
@@ -4,6 +4,7 @@ public class ParqueEolico extends CentralesProductoras {
     private int nivelEmision = 0;
 
 
+
     public ParqueEolico(Propietario p,int capMaxima, int cantGeneradores) {
         super(capMaxima,p);
         this.tipoEnergia = "aire";
@@ -12,27 +13,15 @@ public class ParqueEolico extends CentralesProductoras {
     }
 
 
-
-    public int cantEnergiaAaportar(int cantGeneradores) {
-        int cant;
-        cant =cantGeneradores*13;   //Todos los generadores son iguales y producen 13 de energia
-        return cant;
-        }
+    @Override
+    public int calcularEnergiaProducida() {
+        return cantGeneradores * 13;
+    }
 
     @Override
-    public int nivelEmision(int cantGeneradores) {
+    public int nivelEmision() {
         return 0;
     }
-
-
-    public boolean esContaminante() {
-        boolean es=false;
-        if (nivelEmision >= 70) {
-            es = true;
-        }
-        return es;
-    }
-
 
     public boolean esEficiente(){
         boolean es=false;
@@ -42,8 +31,15 @@ public class ParqueEolico extends CentralesProductoras {
         return es;
     }
 
-    public void modificarCantGeneradores(int cantGeneradores) {
-        this.cantGeneradores = cantGeneradores;
+   public void incrementarGeneradores(){
+        cantGeneradores++;
+   }
+
+    public void decrementarGeneradores(){
+        cantGeneradores--;
+        if(cantGeneradores<=0){
+            cantGeneradores=1;
+        }
     }
 }
 
